4aca15d86f9bf570aebd2058ab5fe0096780959c
AlexisAndre34 <49314347+AlexisAndre34@users.noreply.github.com>
2019-12-29 14:43:29 +0100

    Add files via upload

--- server.R
+++ server.R
@@ -6,22 +6,40 @@
 # 
 #    http://shiny.rstudio.com/
 #
-
+library(readxl)
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(eeptools)
+
+Segment_2019 <- read_excel("Segment-2019.xlsx")
+Qualif_2019 <- read_excel("Qualif 2019.xlsx")
+
+dataProject <- left_join(Qualif_2019,Segment_2019, by="IDENTIFIANT")
+
+ages2019 <- floor(age_calc(as.Date(dataProject$'DATE DE NAISSANCE.x'),Sys.Date(), units = "years"))
+
+dataProject <- cbind(dataProject,ages2019)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
   #colnames(dataProject) <- make.unique(names(dataProject))
   
-  output$distPlot <- renderPlot({
+  output$alexis <- renderPlot({
    moto <- dataProject %>% dplyr::filter(dataProject$SEGMENT %in% input$idtypemoto)
+   age_scale <- as.numeric(input$age)
+   if (is.null(input$idtypemoto)){
+     ggplot(data = dataProject, aes(x = IDENTIFIANT, y = ages2019, color = SEGMENT)) + 
+       geom_point(aes(shape = SEGMENT))
+   }
+   else{
+     ggplot(data = moto, aes(x = IDENTIFIANT, y = ages2019, color = SEGMENT)) + 
+       geom_point(aes(shape = SEGMENT)) +
+       ylim(age_scale[1], age_scale[2])
+   }
    
    
-   ggplot(moto,aes(dataProject$IDENTIFIANT, dataProject$AGE)) + 
-     geom_point()
   })
   
 })
