c18c13df46e421fdfc1a346fc4f891472b6007ab
AlexisAndre34 <49314347+AlexisAndre34@users.noreply.github.com>
2020-01-01 21:25:18 +0100

    Add files via upload

--- server.R
+++ server.R
@@ -1,4 +1,3 @@
-
 #
 # This is the server logic of a Shiny web application. You can run the 
 # application by clicking 'Run App' above.
@@ -13,49 +12,58 @@ library(dplyr)
 library(ggplot2)
 library(eeptools)
 
-Qualif_2019 <- read_excel("C:/Users/haasm/Downloads/Qualif 2019.xlsx")
-
-Segment_2019 <- read_excel("C:/Users/haasm/Downloads/Segment-2019 (2).xlsx")
+Segment_2019 <- read_excel("Segment-2019.xlsx")
+Qualif_2019 <- read_excel("Qualif 2019.xlsx")
 
 dataProject <- left_join(Qualif_2019,Segment_2019, by="IDENTIFIANT")
 
-ages2019 <- floor(age_calc(as.Date(dataProject$`DATE DE NAISSANCE`),Sys.Date(), units = "years"))
+ages2019 <- floor(age_calc(as.Date(dataProject$'DATE DE NAISSANCE.x'),Sys.Date(), units = "years"))
 
 dataProject <- cbind(dataProject,ages2019)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  #colnames(dataProject) <- make.unique(names(dataProject))
+  
   
   output$alexis <- renderPlot({
-    moto <- dataProject %>% dplyr::filter(dataProject$SEGMENT %in% input$idtypemoto)
-    age_scale <- as.numeric(input$age)
-    if (is.null(input$idtypemoto)){
-      ggplot(data = dataProject, aes(x = IDENTIFIANT, y = ages2019, color = SEGMENT)) + 
-        geom_point(aes(shape = SEGMENT))+
-        ylim(age_scale[1], age_scale[2])
-    }
-    else{
-      ggplot(data = moto, aes(x = IDENTIFIANT, y = ages2019, color = SEGMENT)) + 
-        geom_point(aes(shape = SEGMENT)) +
-        ylim(age_scale[1], age_scale[2])
-    }
-    
-    
+   moto <- dataProject %>% dplyr::filter(dataProject$SEGMENT %in% input$idtypemoto)
+   age_scale <- as.numeric(input$age)
+   if (is.null(input$idtypemoto)){
+     ggplot(data = dataProject, aes(x = IDENTIFIANT, y = ages2019, color = SEGMENT)) + 
+       geom_point(aes(shape = SEGMENT))+
+       ylim(age_scale[1], age_scale[2])
+   }
+   else{
+     ggplot(data = moto, aes(x = IDENTIFIANT, y = ages2019, color = SEGMENT)) + 
+       geom_point(aes(shape = SEGMENT)) +
+       ylim(age_scale[1], age_scale[2])
+   }
+   
+   
   })
   
   output$justine <- renderPlot({
-    age_scale <- as.numeric(input$age)
+    observe({
+      age_scale <- as.numeric(input$age)
+    })
+   
     
     #Data frame avec l'age et la categorie de moto 
     df <- data.frame(
       group = ages2019,
       value = c(dataProject$'SEGMENT')
     )
+  
     
+   
+    
+   
+    select_tranche <- filter(df, group >= as.numeric(input$age1[1]) & group <= as.numeric(input$age1[2]))
+
     #Data frame avec l'age compris entre des bornes
-    select_tranche<- filter(df, group >age_scale[1] & group <age_scale[2])
+    
+  
     
     
     #Data frame avec la frequence des moto en fonction de l'age donnee et de la moto
@@ -80,96 +88,5 @@ shinyServer(function(input, output) {
     
     
   })
-  
-  output$Matt <- renderPlot({
-    
-    if (is.null(input$typemoto)){
-      moto <- dataProject
-    }
-    else{
-      moto <- dataProject %>% dplyr::filter(dataProject$SEGMENT %in% input$typemoto)
-    }
-    
-    csp <- moto$`CSP`
-    status <- case_when(
-      csp == "Cadres" ~ "Cadres",
-      csp == "Employés" ~ "Employes",
-      csp == "Etudiants" | csp =="En recherche d'emploi" ~ "Etudiant & Chomeurs",
-      TRUE ~ "Autres"
-    )
-    
-    status <- cbind(status,moto$`SEGMENT`)
-    
-    df4<-data.frame(
-      status = status[,1],
-      modele = status[,2] 
-    )
-    
-    bp4<-ggplot(df4) + geom_bar(aes(x=status,y=modele,fill=modele),stat="identity") 
-    bp4
-  })
-  
-  output$Matt2 <- renderPlot({
-    
-    if (is.null(input$typemoto2)){
-      moto <- dataProject
-    }
-    else{
-      moto <- dataProject %>% dplyr::filter(dataProject$SEGMENT %in% input$typemoto2)
-    }
-    
-    situation <- moto$`SITFAM`
-    
-    situation <- case_when(
-      situation == "En couple sans enfant"| situation =="En couple avec enfant(s)" ~ "En couple",
-      situation == "Seul(e) sans enfant"| situation =="Seul(e) avec enfants" ~ "Celibataire",
-      TRUE ~ "Autres"
-    )
-    
-    situation <- cbind(situation,moto$`SEGMENT`)
-    
-    df3<-data.frame(
-      situation = situation[,1],
-      modele = situation[,2] 
-    )
-    
-    bp3<-ggplot(df3) + geom_bar(aes(x=situation,y=modele,fill=modele),stat="identity")
-    bp3
-  })
-  
-  output$Quantiles <- renderPlot({
-    
-    if (is.null(input$typemoto3)){
-      moto <- dataProject
-      ages <- floor(age_calc(as.Date(moto$`DATE DE NAISSANCE`),Sys.Date(), units = "years"))
-    }
-    else{
-      moto <- dataProject %>% dplyr::filter(dataProject$SEGMENT %in% input$typemoto3)
-      ages <- floor(age_calc(as.Date(moto$`DATE DE NAISSANCE`),Sys.Date(), units = "years"))
-    }
-    
-    
-    ages <- case_when(
-      ages < 39  ~ "0 - 38",
-      ages > 38 & ages < 49 ~ "38 - 48",
-      ages > 48 & ages < 58 ~ "48 - 57",
-      ages > 57 ~ "57 - 95"
-    )
-    
-    
-    ageCompare <- cbind(ages,moto$`SEGMENT`)
-    
-    
-    df6<-data.frame(
-      age = ageCompare[,1],
-      modele = ageCompare[,2] 
-    )
-  
-    
-    bp6<-ggplot(df6) + geom_bar(aes(x=age,y=fct_infreq(modele),fill=modele),stat="identity")
-    bp6
-  })
-  
-  
   
 })
--- ui.R
+++ ui.R
@@ -12,123 +12,39 @@ library(dplyr)
 
 # Define UI for application that draws a histogram
 shinyUI(navbarPage("datscienceproject",
-                   
-                   # Application title
-                   tabPanel("Alexis",
-                            
-                            
-                            # Sidebar with a slider input for number of bins 
-                            sidebarLayout(
-                              sidebarPanel(
-                                checkboxGroupInput("idtypemoto", "Type de moto:",
-                                                   c("Custom" = "Custom",
-                                                     "Quad" = "Quad",
-                                                     "Roadster" = "Roadster",
-                                                     "Routière / GT" = "Routière / GT",
-                                                     "Scooter" = "Scooter",
-                                                     "Side-car / Trike" = "Side-car / Trike",
-                                                     "Sportive" = "Sportive",
-                                                     "Tout terrain (Trial,  Enduro, Cross)" = "Tout terrain (Trial,  Enduro, Cross)",
-                                                     "Trail / Supermotard" = "Trail / Supermotard"
-                                                   )),
-                                sliderInput("age", "slider age :", min = 18, max = 90, value = c(18,90)),
-                                
-                              ),
-                              mainPanel(
-                                plotOutput("alexis")
-                              )
-                            )
-                   ),
-                   tabPanel("justine",
-                            sidebarLayout(
-                              sidebarPanel(
-                                sliderInput("age", "slider age :", min = 18, max = 90, value = c(18,90)),
-                                
-                              ),
-                              mainPanel(
-                                plotOutput("justine")
-                              )
-                            )
-                            
-                            
-                            
-                            
-                            
-                   ),
-                   tabPanel("temil"),
-                   tabPanel("Matt",
-                            sidebarLayout(
-                              sidebarPanel(
-                                checkboxGroupInput("typemoto", "Type de moto:",
-                                                   c("Custom" = "Custom",
-                                                     "Quad" = "Quad",
-                                                     "Roadster" = "Roadster",
-                                                     "Routiere / GT" = "Routiere / GT",
-                                                     "Scooter" = "Scooter",
-                                                     "Side-car / Trike" = "Side-car / Trike",
-                                                     "Sportive" = "Sportive",
-                                                     "Tout terrain (Trial,  Enduro, Cross)" = "Tout terrain (Trial,  Enduro, Cross)",
-                                                     "Trail / Supermotard" = "Trail / Supermotard"
-                                                   )),
-                                
-                              ),
-                              
-                              # Show a plot of the generated distribution
-                              mainPanel(
-                                plotOutput("Matt")
-                              )
-                            )  
-                            
-                            
-                   ),
-                   tabPanel("Matt2",
-                            sidebarLayout(
-                              sidebarPanel(
-                                checkboxGroupInput("typemoto2", "Type de moto:",
-                                                   c("Custom" = "Custom",
-                                                     "Quad" = "Quad",
-                                                     "Roadster" = "Roadster",
-                                                     "Routiere / GT" = "Routiere / GT",
-                                                     "Scooter" = "Scooter",
-                                                     "Side-car / Trike" = "Side-car / Trike",
-                                                     "Sportive" = "Sportive",
-                                                     "Tout terrain (Trial,  Enduro, Cross)" = "Tout terrain (Trial,  Enduro, Cross)",
-                                                     "Trail / Supermotard" = "Trail / Supermotard"
-                                                   )),
-                                
-                              ),
-                              
-                              # Show a plot of the generated distribution
-                              mainPanel(
-                                plotOutput("Matt2")
-                              )
-                            )  
-                            
-                            
-                   ),
-                   tabPanel("Quantiles",
-                            sidebarLayout(
-                              sidebarPanel(
-                                checkboxGroupInput("typemoto3", "Type de moto:",
-                                                   c("Custom" = "Custom",
-                                                     "Quad" = "Quad",
-                                                     "Roadster" = "Roadster",
-                                                     "Routiere / GT" = "Routiere / GT",
-                                                     "Scooter" = "Scooter",
-                                                     "Side-car / Trike" = "Side-car / Trike",
-                                                     "Sportive" = "Sportive",
-                                                     "Tout terrain (Trial,  Enduro, Cross)" = "Tout terrain (Trial,  Enduro, Cross)",
-                                                     "Trail / Supermotard" = "Trail / Supermotard"
-                                                   )),
-                                
-                              ),
-                              
-                              # Show a plot of the generated distribution
-                              mainPanel(
-                                plotOutput("Quantiles")
-                              )
-                            )  
-                            
-                            
-                   )
+  
+  # Application title
+  tabPanel("nuage de point",
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput("idtypemoto", "Type de moto:",
+                         c("Custom" = "Custom",
+                           "Quad" = "Quad",
+                           "Roadster" = "Roadster",
+                           "Routière / GT" = "Routière / GT",
+                           "Scooter" = "Scooter",
+                           "Side-car / Trike" = "Side-car / Trike",
+                           "Sportive" = "Sportive",
+                           "Tout terrain (Trial,  Enduro, Cross)" = "Tout terrain (Trial,  Enduro, Cross)",
+                           "Trail / Supermotard" = "Trail / Supermotard"
+                           )),
+      sliderInput("age", "slider age :", min = 18, max = 90, value = c(18,90)),
+      
+    ),
+    mainPanel(
+       plotOutput("alexis")
+    )
+  )
+  ),
+  tabPanel("justine",
+           sidebarLayout(
+             sidebarPanel(
+               sliderInput("age1", "slider age :", min = 18, max = 90, value = c(18,90)),
+             ),
+             mainPanel(
+               plotOutput("justine")
+             )
+           ))
 ))
