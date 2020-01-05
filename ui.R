a94252e6e344578903478c1f5c0ed0c3bd6922cd
MatthewHaas19 <44777911+MatthewHaas19@users.noreply.github.com>
2020-01-05 16:49:45 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -12,39 +12,93 @@ library(dplyr)
 
 # Define UI for application that draws a histogram
 shinyUI(navbarPage("datscienceproject",
-  
-  # Application title
-  tabPanel("nuage de point",
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      checkboxGroupInput("idtypemoto", "Type de moto:",
-                         c("Custom" = "Custom",
-                           "Quad" = "Quad",
-                           "Roadster" = "Roadster",
-                           "Routière / GT" = "Routière / GT",
-                           "Scooter" = "Scooter",
-                           "Side-car / Trike" = "Side-car / Trike",
-                           "Sportive" = "Sportive",
-                           "Tout terrain (Trial,  Enduro, Cross)" = "Tout terrain (Trial,  Enduro, Cross)",
-                           "Trail / Supermotard" = "Trail / Supermotard"
-                           )),
-      sliderInput("age", "Choisir la tranche d'age :", min = 18, max = 90, value = c(18,90)),
-      
-    ),
-    mainPanel(
-       plotOutput("alexis")
-    )
-  )
-  ),
-  tabPanel("Repartition Moto selon l'age",
-           sidebarLayout(position = "left",
-             sidebarPanel(
-               sliderInput("age1", "Choisir la tranche d'age :", min = 18, max = 92, value = c(18,92)),
-             ),
-             mainPanel(
-               plotOutput("motoSelonAge")
-             )
-           ))
+                   
+                   # Application title
+                   tabPanel("nuage de point",
+                            
+                            # Sidebar with a slider input for number of bins 
+                            sidebarLayout(
+                              sidebarPanel(
+                                checkboxGroupInput("idtypemoto", "Type de moto:",
+                                                   c("Custom" = "Custom",
+                                                     "Quad" = "Quad",
+                                                     "Roadster" = "Roadster",
+                                                     "Routière / GT" = "Routière / GT",
+                                                     "Scooter" = "Scooter",
+                                                     "Side-car / Trike" = "Side-car / Trike",
+                                                     "Sportive" = "Sportive",
+                                                     "Tout terrain (Trial,  Enduro, Cross)" = "Tout terrain (Trial,  Enduro, Cross)",
+                                                     "Trail / Supermotard" = "Trail / Supermotard"
+                                                   )),
+                                sliderInput("age", "Choisir la tranche d'age :", min = 18, max = 90, value = c(18,90)),
+                                
+                              ),
+                              mainPanel(
+                                plotOutput("alexis")
+                              )
+                            )
+                   ),
+                   tabPanel("Repartition Moto selon l'age",
+                            sidebarLayout(position = "left",
+                                          sidebarPanel(
+                                            sliderInput("age1", "Choisir la tranche d'age :", min = 18, max = 92, value = c(18,92)),
+                                          ),
+                                          mainPanel(
+                                            plotOutput("motoSelonAge")
+                                          )
+                            )),
+                   tabPanel("temil"),
+                   tabPanel("Catégorie socio professionnelle",
+                            sidebarLayout(
+                              sidebarPanel(
+                                checkboxGroupInput("typemoto", "Type de moto:",
+                                                   c("Custom" = "Custom",
+                                                     "Quad" = "Quad",
+                                                     "Roadster" = "Roadster",
+                                                     "Routiere / GT" = "Routiere / GT",
+                                                     "Scooter" = "Scooter",
+                                                     "Side-car / Trike" = "Side-car / Trike",
+                                                     "Sportive" = "Sportive",
+                                                     "Tout terrain (Trial,  Enduro, Cross)" = "Tout terrain (Trial,  Enduro, Cross)",
+                                                     "Trail / Supermotard" = "Trail / Supermotard"
+                                                   )),
+                                
+                              ),
+                              
+                              # Show a plot of the generated distribution
+                              mainPanel(
+                                plotOutput("Matt")
+                              )
+                            )  
+                            
+                            
+                   ),
+                   tabPanel("Situation familliale",
+                            sidebarLayout(
+                              sidebarPanel(
+                                checkboxGroupInput("typemoto2", "Type de moto:",
+                                                   c("Custom" = "Custom",
+                                                     "Quad" = "Quad",
+                                                     "Roadster" = "Roadster",
+                                                     "Routiere / GT" = "Routiere / GT",
+                                                     "Scooter" = "Scooter",
+                                                     "Side-car / Trike" = "Side-car / Trike",
+                                                     "Sportive" = "Sportive",
+                                                     "Tout terrain (Trial,  Enduro, Cross)" = "Tout terrain (Trial,  Enduro, Cross)",
+                                                     "Trail / Supermotard" = "Trail / Supermotard"
+                                                   )),
+                                
+                              ),
+                              
+                              # Show a plot of the generated distribution
+                              mainPanel(
+                                plotOutput("Matt2")
+                              )
+                            )  
+                            
+                            
+                   )    
 ))
+
+
+
